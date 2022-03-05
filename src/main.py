6603ee64b32df3a62e42e384cdf3795cf62a7197
WIN-A9HG52NFJ3P\Dimasik <dima-nru@mail.ru>
2022-03-05 23:01:41 +0400

    переделаны макеты сайтов под bootstrap

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
+from . import models
+from .models import User
 
 main = Blueprint('main', __name__)
 
@@ -10,7 +12,8 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/profile')
+@main.route('/workallusers')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    users = User.query.filter_by().all()
+    return render_template('workallusers.html', users=users)

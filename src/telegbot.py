49707c1cff48cf0c1923480d4946cd840cb66c5d
WIN-A9HG52NFJ3P\Dimasik <dima-nru@mail.ru>
2022-03-12 13:51:25 +0400

    Вроде телеграм бот доделан осталось проверить

--- src/telegbot.py
+++ src/telegbot.py
@@ -2,7 +2,7 @@ import telebot
 import re
 
 from .models import *
-from . import init_db
+from . import init_db, db
 
 tgbot = telebot.TeleBot('5162461863:AAGpVulVti1UpHHx-kPBB4-CNZT9mpvfk6Q')
 phone_num = re.compile('^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$')
@@ -12,35 +12,62 @@ def listen_tg_bot():
     tgbot.polling(none_stop=True, interval=1)
 
 
-def send_message(text, chat_id, message_id):
+async def send_message(text, chat_id, message_id):
     keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(telebot.types.InlineKeyboardButton(text="Получил", callback_data=message_id))
-    await tgbot.send_message("Пожалуйста нажмите на кнопку получил, чтобы подтвердить что вы получили сообщение", reply_markup=keyboard)
+    keyboard.add(telebot.types.InlineKeyboardButton(text="Получил", callback_data=f'msg{message_id}'))
+    await tgbot.send_message("Пожалуйста нажмите на кнопку получил, чтобы подтвердить что вы получили сообщение",
+                             reply_markup=keyboard)
+
+
+@tgbot.callback_query_handler(regexp='^msg(\d{1,10})$')
+async def mark_msg(call: telebot.types.CallbackQuery):
+    with init_db().app_context():
+        chat_id = call.message.chat.id
+        message_id = re.match('(\d{1,10})').string
+        user = User.query.filter_by(tlg_chat_id=chat_id).first()
+        if user:
+            message = Message.query.filter_by(id=message_id).first()
+            if message:
+                Message.query.filter_by(id=message_id).update({'tlg_received': True})
+                db.session.commit()
 
 
 @tgbot.message_handler(commands=['reg'])
-def reg_command(message):
+async def reg_command(message):
     with init_db().app_context():
         user = User.query.filter_by(tlg_chat_id=message.from_user.id).first()
         if not user:
-            tgbot.send_message(message.from_user.id, "Введите свой номер телефона в формате 8 9** *** ** ** :")
+            await tgbot.send_message(message.from_user.id, "Введите свой номер телефона в формате 8 9** *** ** ** :")
         else:
-            tgbot.send_message(user.tlg_chat_id,
-                               f"Вы уже выполнили вход как пользователь: {user.name} с номером телефона: {user.phone_number} вы: {user.app_role}")
+            await tgbot.send_message(user.tlg_chat_id,
+                                     f"Вы уже выполнили вход как пользователь: {user.name} с номером телефона: {user.phone_number} вы: {user.app_role}")
 
 
 @tgbot.message_handler(regexp='^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$')
-def phone_number(message):
-    s = phone_num.match(message.text).string.replace(' ', '').replace('-','')
-    tgbot.send_message(message.from_user.id, s)
+async def phone_number(message):
+    with init_db().app_context():
+        s = phone_num.match(message.text).string.replace(' ', '').replace('-', '')
+        user = User.query.filter_by(phone_number=s).first()
+        if user and not user.tlg_authorized:
+            User.query.filter_by(phone_number=s).update({'tlg_authorized': True, 'lg_chat_id': message.chat.id})
+            db.session.commit()
+            await message.reply(f'Вы успешно авторизованы как пользователь {user.name} {user.phone_number}')
+        elif user and user.tlg_authorized and user.tlg_chat_id == message.chat.id:
+            await message.reply(f'Вы уже авторизованы как пользователь {user.name} {user.phone_number}')
+        elif user and user.tlg_authorized and user.tlg_chat_id != message.chat.id:
+            await message.reply('Пользователь с таким номером телефона уже авторизован в системе')
+        else:
+            await message.reply('Пользователь с таким номером телефона еще не добавлен администратором, обратитесь к администратору для решения этого вопроса')
 
 
 @tgbot.message_handler(regexp='((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}')
-def phone_number(message):
+async def phone_number_wrong(message):
     s = phone_num.match(message.text)
-    tgbot.send_message(message.from_user.id, 'неверно введен номер телефона, строка не должна содержать иных символов и слов кроме номера телефона')
+    await tgbot.send_message(message.from_user.id,
+                             'неверно введен номер телефона, строка не должна содержать иных символов и слов кроме номера телефона')
+
 
 @tgbot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    tgbot.send_message(message.from_user.id,
-                       "Если вы еще не выполнили вход: отправьте команду /reg\n, иначе просто периодически проверяйте входящие сообщения")
+async def get_text_messages(message):
+    await tgbot.send_message(message.from_user.id,
+                             "Если вы еще не выполнили вход: отправьте команду /reg\n, иначе просто периодически проверяйте входящие сообщения")
